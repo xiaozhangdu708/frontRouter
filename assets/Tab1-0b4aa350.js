7a6aa23abd93907dc3b7d77946df401c80afa8a0
xiaozhangdu708 <132722989+xiaozhangdu708@users.noreply.github.com>
2025-06-10 07:44:20 +0800

    Add files via upload

---
+++ assets/Tab1-0b4aa350.js
@@ -0,0 +1,1 @@
+import{_ as e,o as _,c}from"./index-cda2acb5.js";const a={};function t(n,o){return _(),c("div",null,"切换栏组件1")}const r=e(a,[["render",t],["__scopeId","data-v-5187ae8c"]]);export{r as default};
---
+++ assets/Tab2-63c50c41.js
@@ -0,0 +1,1 @@
+import{_ as e,o as _,c}from"./index-cda2acb5.js";const t={};function n(o,a){return _(),c("div",null,"切换栏组件2")}const r=e(t,[["render",n],["__scopeId","data-v-c7524155"]]);export{r as default};
